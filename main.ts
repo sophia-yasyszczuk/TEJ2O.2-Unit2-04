28dfcf3c6d930b6d6ccf644fdde32f8b21c7260a
sophia-yasyszczuk <sophia.yasyszczuk@stu.ocsb.ca>
2026-02-24 13:10:20 -0500

    Made the microbit say the temperature

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-    let temperature = input.temperature()
+    const temperature = input.temperature()
     basic.clearScreen()
     basic.showString("the temperature is:" + temperature + " C.")
 })
